c69b150e9d2307584d84bf52872e566341a66be3
RayLonscholar <f82621672@gmail.com>
2025-12-16 00:07:34 +0800

    fix(read_excel): fix the problem of reading file error

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import platform
+import sys
 
 import dotenv
 import flet as ft
@@ -8,15 +10,27 @@ from src.ui.views.main_view import MainView
 
 
 def main(page: ft.Page):
-    # 1. 基礎設定
+    # 1. 初始設定
+    dotenv.load_dotenv()
+    system = platform.system()
+    print("Operating System:", system)
+    if getattr(sys, "frozen", False):
+        if system == "Linux":
+            try:
+                os.environ["ROOT_PATH"] = os.path.dirname(os.readlink("/proc/self/exe"))
+            except Exception:
+                pass
+        else:
+            os.environ["ROOT_PATH"] = os.path.dirname(os.path.abspath(sys.executable))
+    else:
+        os.environ["ROOT_PATH"] = os.path.dirname(os.path.abspath(__file__))
+
+    # 2. 基礎設定
     page.title = "自動點名系統-中國科技大學(專用)"
     page.theme = get_dark_theme()
     page.theme_mode = ft.ThemeMode.DARK
     page.window.width = 400
     page.window.height = 700
-
-    dotenv.load_dotenv()
-    os.environ["ROOT_PATH"] = os.path.dirname(os.path.abspath(__file__))
 
     # 2. 載入 View
     # 這裡示範最簡單的直接掛載，若專案較大建議使用 page.on_route_change
